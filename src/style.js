c05f80114c0327f8e96790bade26b5a6f93f5040
Pradyun Gayakwad <pradyungayakwad@gmail.com>
2022-11-26 18:40:13 +0530

    Added Image Gallary

--- src/style.js
+++ src/style.js
@@ -1,5 +1,6 @@
 const styles = {
   boxWidth: "xl:max-w-[1280px] w-full",
+  innerWidth: '2xl:max-w-[1280px] w-full',
 
   heading2: "font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full",
   paragraph: "font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]",
@@ -10,6 +11,7 @@ const styles = {
   paddingX: "sm:px-16 px-6",
   paddingY: "sm:py-16 py-6",
   padding: "sm:px-16 px-6 sm:py-12 py-4",
+  paddings: 'sm:p-16 xs:p-8 px-6 py-12',
 
   marginX: "sm:mx-16 mx-6",
   marginY: "sm:my-16 my-6",
