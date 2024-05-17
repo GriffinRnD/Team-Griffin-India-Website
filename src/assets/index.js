18af607ca808425a349c089fdf7ae696c953de20
Pradyun Gayakwad <pradyungayakwad@gmail.com>
2024-05-17 09:30:59 +0530

    updating details

--- src/assets/index.js
+++ src/assets/index.js
@@ -24,6 +24,9 @@ import people01 from "./people01.jpg";
 import people02 from "./people02.jpg";
 import people03 from "./people03.jpg";
 import people04 from "./people04.jpg";
+import people05 from "./people05.jpg";
+import people06 from "./people06.jpg";
+import people07 from "./people07.jpg";
 import plane from "./plane.png";
 import sponsor from "./spons.png";
 import sponsor1 from "./SolidWorks-Logo.png";
@@ -66,6 +69,9 @@ export {
   people02,
   people03,
   people04,
+  people05,
+  people06,
+  people07,
   plane,
   sponsor,
   sponsor1,
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-import { people01, people02, people03, people04, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star,GrifPic1,GrifPic2,GrifPic3,GrifPic4,GrifPic5,  } from "../assets";
+import { people01, people02, people03, people04, people05, people06, people07, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star,GrifPic1,GrifPic2,GrifPic3,GrifPic4,GrifPic5,  } from "../assets";
 
 export const navLinks = [
   {
@@ -76,7 +76,7 @@ export const feedback = [
     id: "feedback-1",
     content:
       "Where love for Aviation takes flight...",
-    name: "Jay Pargaonkar",
+    name: "Aditya Matale",
     title: "Captain",
     img: people01,
   },
@@ -84,17 +84,49 @@ export const feedback = [
     id: "feedback-2",
     content:
       "Group of really Hard-Working and Talented guys.",
-    name: "Anish Parke",
+    name: "Aniket Dharmadhikari",
     title: "Vice-Captain",
     img: people02,
   },
   {
     id: "feedback-3",
     content:
-      "We Bleed Cyno.",
-    name: "Hrutuj",
+      "A Place Where There Are No Limits.",
+    name: "Pradyun Gayakwad",
+    title: "Team Manager",
+    img: people03,
+  },
+  {
+    id: "feedback-4",
+    content:
+      "We Work, We Build, We Fly",
+    name: "Yadhnesh C.",
     title: "Team Manager",
     img: people04,
+  },
+  {
+    id: "feedback-5",
+    content:
+      "It Aint Gonna Pay For Itself",
+    name: "Sayonee Dhepe",
+    title: "Treasurer",
+    img: people05,
+  },
+  {
+    id: "feedback-6",
+    content:
+      "We Give You Wings Without Wings",
+    name: "Prasanna Gebise",
+    title: "Multirotor Captain",
+    img: people06,
+  },
+  {
+    id: "feedback-7",
+    content:
+      "We Make Concepts That Undermine Reality",
+    name: "Arya Pimpalgaonkar",
+    title: "RnD Chief",
+    img: people07,
   },
 ];
 
@@ -102,7 +134,7 @@ export const stats = [
   {
     id: "stats-1",
     title: "Number of Active Members",
-    value: "40+",
+    value: "60+",
   },
   {
     id: "stats-2",
@@ -147,7 +179,7 @@ export const footerLinks = [
     links: [
       {
         name: "Whatsapp",
-        link: "https://wa.me/917022671930",
+        link: "https://wa.me/919823630040",
       },
       {
         name: "Email",
@@ -155,15 +187,15 @@ export const footerLinks = [
       },
       {
         name: "Contact 1",
-        link: "tel:917022671930",
+        link: "tel:919823630040",
       },
       {
         name: "Contact 2",
-        link: "tel:918956909950",
+        link: "tel:917507926565",
       },
       {
         name: "Contact 3",
-        link: "tel:918806076149",
+        link: "tel:919145328396",
       },
       // {
       //   name: "Newsletters",
